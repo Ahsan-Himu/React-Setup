f37329e9425303053ce24ea80ea7c6b4d66313a7
Ahsan Himu <ahsanhimuofficial@gmail.com>
2024-07-11 10:12:46 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='text-green-800 font-bold text-4xl items-center pt-48'>React setup successful!</h1>
+    </div>
+  );
+}
+
+export default App;
